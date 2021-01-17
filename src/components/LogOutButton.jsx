fc3745148c201c183d9adbd2da4e54d35d83b1a9
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-17 19:46:40 +0900

    add: ログアウトの実装

---
+++ src/components/LogOutButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { TouchableOpacity, Text, StyleSheet, Alert } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import firebase from 'firebase'
+
+export default function LogOutButton() {
+  const navigation = useNavigation()
+
+  function handlePress() {
+    firebase
+      .auth()
+      .signOut()
+      // ログアウトに成功したとき
+      .then(() => {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'LogIn' }],
+        })
+      })
+      // ログアウトに失敗したとき
+      .catch(() => {
+        Alert.alert('ログアウトに失敗しました')
+      })
+  }
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
+      <Text style={styles.label}>ログアウト</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  label: {
+    fontSize: 14,
+    color: 'rgba(255,255,255,0.7)',
+  },
+})
